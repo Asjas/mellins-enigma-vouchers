d2fbfb9614bab44aab7d0f1d248908acf3b88d75
Asjas <asjas@outlook.com>
2019-08-17 00:02:41 +0200

    Update MJML Voucher email
    Fix Google Fonts loading duplicate Roboto fonts.
    Set Voucher font color to white.

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -36,8 +36,8 @@ export async function sendMail(email: string, voucher: string): Promise<void> {
     <mjml>
       <mj-head>
         <mj-title>Mellins iStyle Voucher</mj-title>
-        <mj-font name="raleway" href="https://fonts.googleapis.com/css?family=Raleway" />
-        <mj-font name="roboto" href="https://fonts.googleapis.com/css?family=Roboto" />
+        <mj-font name="Raleway" href="https://fonts.googleapis.com/css?family=Raleway" />
+        <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Roboto" />
       </mj-head>
       <mj-body>
 
@@ -45,23 +45,23 @@ export async function sendMail(email: string, voucher: string): Promise<void> {
           <mj-column>
             <mj-image width="240px" height="40px" src="https://cdn.shopify.com/s/files/1/0045/5632/4936/files/Mellins_top_logo.png?10964"></mj-image>
             <mj-divider border-color="#445a6a"></mj-divider>
-            <mj-text font-size="20px" color="#445a6a" font-family="roboto">Voucher</mj-text>
-            <mj-text font-size="14px" color="#445a6a" padding-bottom="25px" font-family="raleway">Here is your voucher number.</mj-text>
-            <mj-text font-size="30px" align="center" padding="20px" font-family="roboto" container-background-color="#445a6a" >#${voucher}</mj-text>
+            <mj-text font-size="20px" color="#445a6a" font-family="Roboto">Voucher</mj-text>
+            <mj-text font-size="14px" color="#445a6a" padding-bottom="25px" font-family="Raleway">Here is your voucher number.</mj-text>
+            <mj-text font-size="30px" align="center" padding="20px" font-family="Roboto" color="#ffffff" container-background-color="#445a6a">#${voucher}</mj-text>
           </mj-column>
         </mj-section>
 
         <mj-section background-color="#e7e7e7">
           <mj-column>
-            <mj-text font-size="16px" align="center" padding-bottom="5px" font-family="roboto">Mellins iStyle</mj-text>
-            <mj-text align="center" padding-bottom="0" font-family="raleway">Phone: (010) 100 3310</mj-text>
-            <mj-text align="center" padding-bottom="0" font-family="raleway"> Email: info@mellins.co.za</mj-text>
+            <mj-text font-size="16px" align="center" padding-bottom="5px" font-family="Roboto">Mellins iStyle</mj-text>
+            <mj-text align="center" padding-bottom="0" font-family="Raleway">Phone: (010) 100 3310</mj-text>
+            <mj-text align="center" padding-bottom="0" font-family="Raleway"> Email: info@mellins.co.za</mj-text>
           </mj-column>
           <mj-column>
-            <mj-text font-size="16px" align="center" font-family="roboto">Follow us:</mj-text>
+            <mj-text font-size="16px" align="center" font-family="Roboto">Follow us:</mj-text>
             <mj-social>
-              <mj-social-element name="facebook" href="https://www.facebook.com/mellinsistyle/" alt="facebook" />
               <mj-social-element name="twitter" href="https://twitter.com/MellinsiStyle" alt="twitter" />
+              <mj-social-element name="facebook" href="https://www.facebook.com/mellinsistyle/" alt="facebook" />
               <mj-social-element name="instagram" href="https://www.instagram.com/mellinsistyle/?hl=en" alt="instagram" />
             </mj-social>
           </mj-column>
