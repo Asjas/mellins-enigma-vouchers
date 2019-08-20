1157d01708b746fd741a264715b66191fc5c9d79
Asjas <asjas@outlook.com>
2019-08-20 16:43:19 +0200

    Add enigma post request to create a voucher

--- src/controllers/voucher.ts
+++ src/controllers/voucher.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import axios from 'axios';
 
 import { sendMail } from '../services/nodemailer';
 
@@ -19,11 +20,39 @@ export async function voucher(req: Request, res: Response): Promise<void> {
   const { email, param } = req.body as { email: string; param: string };
   console.log(email);
   console.log(param);
-  const voucherId = '2000';
 
   if (email) {
-    res.status(200).send('Success. Sending email containing the voucher code.');
+    const auth = Buffer.from('dawidp@pienaarpartners.co.za:Enigma@007').toString('base64');
+    const bearer = `Basic ${auth}`;
+    const tenant = 'mellins';
+    const definitionId = '5c162225995900346de27ba0';
 
-    await sendMail(email, voucherId);
+    await axios({
+      method: 'post',
+      baseURL: 'https://enigma-api.encentivize.co.za/',
+      url: `${tenant}/definitions/${definitionId}/vouchers`,
+      headers: { Authorization: bearer },
+      data: {
+        externalReferenceCode: email,
+        definition: {
+          voucherType: {
+            valueAmount: 120,
+          },
+        },
+      },
+    })
+      .then(response => {
+        const { voucherCode } = response.data;
+
+        console.log(response.data);
+        console.log(response.data.voucherBatchId);
+
+        res.status(201).send('Success. Sending email containing the voucher code.');
+        sendMail(email, voucherCode);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('There was an error with generating the voucher.');
+      });
   }
 }
